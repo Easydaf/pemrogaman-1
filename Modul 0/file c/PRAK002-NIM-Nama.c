80427ba4a0c933f096dbc5a1618e7724017bc628
Easydaf <144783125+Easydaf@users.noreply.github.com>
2023-09-14 08:06:57 +0800

    menambahkan file tugas

---
+++ Modul 0/file c/PRAK002-NIM-Nama.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, Muhammad Daffa Musyafa\nSelamat Siang, Muhammad Daffa Musyafa\nSelamat Malam, Muhammad Daffa Musyafa");
+    return 0;
+}
---
+++ Modul 0/file py/PRAK002-NIM-Nama.py
@@ -0,0 +1,7 @@
+x = ("Selamat Pagi, Muhammad Daffa Musyafa")
+y = ("Selamat Siang, Muhammad Daffa Musyafa")
+z = ("Selamat Malam, Muhammad Daffa Musyafa")
+
+print(x)
+print(y)
+print(z)
