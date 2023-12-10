75d8ded9ddaf761677d4f696bad9d79e7e06ac4b
Easydaf <144783125+Easydaf@users.noreply.github.com>
2023-12-10 09:40:28 +0800

    Delete Modul 1 directory

--- Modul 1/file python/PRAK102-2310817110007-Muhammad_Daffa_Musayafa.py
+++
@@ -1,9 +0,0 @@
-a = 4
-b = 8
-c = 3
-hasil = (a * b) / c
-d = round (hasil, 6)
-print('variabel a bernilai', a)
-print('variabel b bernilai', b)
-print('variabel c bernilai', c)
-print('Hasil dari a dikali b dibagi c adalah', d)
--- Modul 1/file python/PRAK109-2310817110007-Muhammad_Daffa_Musyafa.py
+++
@@ -1,6 +0,0 @@
-pasukan_yz = 958.730
-alter_dragon = 5
-pasukan_yang_dikalahkan_oleh_alterians = pasukan_yz / alter_dragon
-print(f'Jumlah pasukan yang dibawa Yu Zhong = ? {pasukan_yz:.3f}')
-print(f'Jumlah pahlawan = ? {alter_dragon}')
-print(f'Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {pasukan_yang_dikalahkan_oleh_alterians}')
