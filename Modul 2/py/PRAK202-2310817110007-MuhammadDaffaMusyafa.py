5d10b4ec85068e416bf7df0668f807f65a1489b9
Easydaf <144783125+Easydaf@users.noreply.github.com>
2023-12-10 09:40:36 +0800

    Delete Modul 2 directory

--- Modul 2/py/PRAK202-2310817110007-MuhammadDaffaMusyafa.py
+++
@@ -1,4 +0,0 @@
-a =float(input("Masukkan Nilai Pertama :"))
-b =float(input("Masukkan Nilai kedua :"))
-c =(float((a) +  (b)))
-print("Hasil dari penjumlahan nilai pertama \"%g\" dan nilai kedua \"%g\" adalah \"%.2f\"" % (a, b, c))
--- Modul 2/py/PRAK203-2310817110007-MuhammadDaffaMusyafa.py
+++
@@ -1,7 +0,0 @@
-a, b, i, j, x, y = input("").split(" ")
-
-
-hasil =(int(a) - int(b)) * int(i) / int(j) - (int(x) + int(y))
-hasil_bulat = round(hasil,3)
-
-print("%.3f"% (hasil_bulat))
